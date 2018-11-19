87e681d7c1b30a35144ba44719345aa82fb7f4ee
Alfie Wilmot <alfred.wilmot@students.plymouth.ac.uk>
2018-11-19 14:03:20 +0000

    HSV inRange working with trackbars

--- src/dilate_erode_stuff.cpp
+++ src/dilate_erode_stuff.cpp
@@ -56,16 +56,18 @@ int const max_kernel_size = 21;
 
 void doErode(int, void *);
 void doDilate(int, void *);
-void ErodeWithTrackbars(void);
-void DilateWithTrackbars(void);
+void Erode_trackbars(void);
+void Dilate_trackbars(void);
 
 /*----------------*/
 /* inRange stuff */ 
 /*--------------*/
 //reference here: https://docs.opencv.org/3.4/da/d97/tutorial_threshold_inRange.html
-char windowName0_[16] = "BlurWindow";
-char windowName1_[16] = "ErodeWindow";
-char windowName2_[16] = "DilateWindow";
+char srcFrameFeed[16] = "Original frame";
+char Blur_Window[16] = "BlurWindow";
+char Erode_Window[16] = "ErodeWindow";
+char Dilate_Window[16] = "DilateWindow";
+char HSV_thresh_window[16] = "HSV Threash";
 
 
 
@@ -80,14 +82,14 @@ void doBlur(int sliderBarVal, void * = NULL)
     }
     /* Slidebar adjusts size of blurring kernel masked over input Mat pixels */
     blur(*src, Blur, Size(sliderBarVal, sliderBarVal));
-    imshow(&windowName0_[0], Blur);
+    imshow(Blur_Window, Blur);
     printf("Blur kernel-size: %d x %d\n\r", sliderBarVal, sliderBarVal);
 }
 void BlurWithTrackbar()
 {
     doBlur(blur_qty);
-    imshow(&windowName0_[0], Blur);
-    createTrackbar("Blur value", &windowName0_[0], &blur_qty, blur_max, doBlur);
+    imshow(Blur_Window, Blur);
+    createTrackbar("Blur value", Blur_Window, &blur_qty, blur_max, doBlur);
 }
 
 /*-----------------------*/
@@ -113,14 +115,14 @@ void doErode(int erosion_elem, void * = NULL)
                                         Size(2 * erosion_size + 1, 2 * erosion_size + 1),
                                         Point(erosion_size, erosion_size));
     erode(*src, erosion_dst, element);
-    imshow(&windowName1_[0], erosion_dst);
+    imshow(Erode_Window, erosion_dst);
 }
-void ErodeWithTrackbars()
+void Erode_trackbars()
 {
-    doErode(erosion_elem);
-    imshow(&windowName1_[0], erosion_dst);
-    createTrackbar("Element:\n 0: Rect \n 1: Cross \n 2: Ellipse", &windowName1_[0], &erosion_elem, max_elem, doErode);
-    createTrackbar("Kernel size:\n 2n +1", &windowName1_[0], &erosion_size, max_kernel_size, doErode);
+    //doErode(erosion_elem);
+    //imshow(Erode_Window, erosion_dst);
+    createTrackbar("Element:\n 0: Rect \n 1: Cross \n 2: Ellipse", Erode_Window, &erosion_elem, max_elem, doErode);
+    createTrackbar("Kernel size:\n 2n +1", Erode_Window, &erosion_size, max_kernel_size, doErode);
 }
 
 /*-----------------------------*/
@@ -146,14 +148,14 @@ void doDilate(int dilation_elem, void * = NULL)
                                         Size(2 * dilation_size + 1, 2 * dilation_size + 1),
                                         Point(dilation_size, dilation_size));
     dilate(*src, dilation_dst, element);
-    imshow(&windowName2_[0], dilation_dst);
+    imshow(Dilate_Window, dilation_dst);
 }
-void DilateWithTrackbars()
+void Dilate_trackbars()
 {
-    doDilate(dilation_elem);
-    imshow(&windowName2_[0], dilation_dst);
-    createTrackbar("Element:\n 0: Rect \n 1: Cross \n 2: Ellipse", &windowName2_[0], &dilation_elem, max_elem, doDilate);
-    createTrackbar("Kernel size:\n 2n +1", &windowName2_[0], &dilation_size, max_kernel_size, doDilate);
+    //doDilate(dilation_elem);
+    //imshow(Dilate_Window, dilation_dst);
+    createTrackbar("Element:\n 0: Rect \n 1: Cross \n 2: Ellipse", Dilate_Window, &dilation_elem, max_elem, doDilate);
+    createTrackbar("Kernel size:\n 2n +1", Dilate_Window, &dilation_size, max_kernel_size, doDilate);
 }
 
 /*-------------------------------------------------*/
@@ -162,6 +164,66 @@ void DilateWithTrackbars()
 // Create trackbars.
 // Convert from BGR-to-HSV.
 // Detect object based on HSV range values.
+const int max_value_H = 360/2;
+const int max_value = 255;
+const String window_capture_name = "Video Capture";
+const String window_detection_name = "Object Detection";
+int low_H = 0, low_S = 0, low_V = 0;
+int high_H = max_value_H, high_S = max_value, high_V = max_value;
+
+Mat frame_HSV, frame_threshold;
+
+static void on_low_H_thresh_trackbar(int, void *)
+{
+    low_H = min(high_H-1, low_H);
+    setTrackbarPos("Low H", window_detection_name, low_H);
+}
+static void on_high_H_thresh_trackbar(int, void *)
+{
+    high_H = max(high_H, low_H+1);
+    setTrackbarPos("High H", window_detection_name, high_H);
+}
+static void on_low_S_thresh_trackbar(int, void *)
+{
+    low_S = min(high_S-1, low_S);
+    setTrackbarPos("Low S", window_detection_name, low_S);
+}
+static void on_high_S_thresh_trackbar(int, void *)
+{
+    high_S = max(high_S, low_S+1);
+    setTrackbarPos("High S", window_detection_name, high_S);
+}
+static void on_low_V_thresh_trackbar(int, void *)
+{
+    low_V = min(high_V-1, low_V);
+    setTrackbarPos("Low V", window_detection_name, low_V);
+}
+static void on_high_V_thresh_trackbar(int, void *)
+{
+    high_V = max(high_V, low_V+1);
+    setTrackbarPos("High V", window_detection_name, high_V);
+}
+
+void init_HSV_trackbars()
+{
+    // Trackbars to set thresholds for HSV values
+    createTrackbar("Low H", HSV_thresh_window, &low_H, max_value_H, on_low_H_thresh_trackbar);
+    createTrackbar("High H", HSV_thresh_window, &high_H, max_value_H, on_high_H_thresh_trackbar);
+    createTrackbar("Low S", HSV_thresh_window, &low_S, max_value, on_low_S_thresh_trackbar);
+    createTrackbar("High S", HSV_thresh_window, &high_S, max_value, on_high_S_thresh_trackbar);
+    createTrackbar("Low V", HSV_thresh_window, &low_V, max_value, on_low_V_thresh_trackbar);
+    createTrackbar("High V", HSV_thresh_window, &high_V, max_value, on_high_V_thresh_trackbar);
+}
+void run_HSV_thresh()
+{
+    // Convert from BGR to HSV colorspace
+    cvtColor(frame, frame_HSV, COLOR_BGR2HSV);
+    // Detect the object based on HSV Range Values
+    inRange(frame_HSV, Scalar(low_H, low_S, low_V), Scalar(high_H, high_S, high_V), frame_threshold);
+    // Show the frames
+    imshow(srcFrameFeed, frame);
+    imshow(HSV_thresh_window, frame_threshold);
+}
 
 
 /* Some reference code: https://raw.githubusercontent.com/kylehounslow/opencv-tuts/master/object-tracking-tut/objectTrackingTut.cpp */
@@ -176,16 +238,16 @@ int main(int, char **)
     /* SETUP: Initialize trackbars and windows */
     /*----------------------------------------*/
 
-    namedWindow(&windowName1_[0]);
-
-
-    cap >> frame; 
+    //namedWindow(Erode_Window);  
+    //namedWindow(Dilate_Window);  
+    namedWindow(srcFrameFeed);
+    namedWindow(HSV_thresh_window);
 
     //cvtColor(frame, GrayScale, COLOR_BGR2GRAY); //Alters the frame data to gray and stores in matrix GrayScale
     //GaussianBlur(GrayScale, Blur, Size(7, 7), 1.5, 1.5); //Alters the GrayScale data to have guassian blur and stores in matrix Blur
     //Canny(Blur, CannyEdit, 0, 30, 3);                    //Updates the altered Blur data to have canny lines and stores in matrix canny edit
     //cvtColor(frame, RGB_to_HSV, COLOR_RGB2HSV);
-    cvtColor(frame, BGR_to_HSV, COLOR_BGR2HSV);
+    //cvtColor(frame, BGR_to_HSV, COLOR_BGR2HSV);
 
     //imshow("Stdfeed", frame);       //Opens a window called feed, containing the camera frame matrix data
     //imshow("GrayScale", GrayScale); //Opens a window called feed, containing the camera frame matrix data
@@ -194,18 +256,19 @@ int main(int, char **)
     //imshow("RGB_to_HSV", RGB_to_HSV);
     //imshow("BGR_to_HSV", BGR_to_HSV);
 
-    //BlurWithTrackbar();
-    ErodeWithTrackbars();
-    DilateWithTrackbars();
-
+    Erode_trackbars();
+    Dilate_trackbars();
+    init_HSV_trackbars();
     while (1)
     {   
 
         cap >> frame; // get a new frame from video capture and store in matrix frame.
 
         cvtColor(frame, BGR_to_HSV, COLOR_BGR2HSV);
-        doErode(erosion_elem);
-        doDilate(dilation_elem);
+        //doErode(erosion_elem);
+        //doDilate(dilation_elem);
+        run_HSV_thresh();
+
 
         if (waitKey(30) >= 0)
         {
