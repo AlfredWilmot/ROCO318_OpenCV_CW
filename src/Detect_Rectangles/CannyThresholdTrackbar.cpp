e216d4172c966449f8953a1ca69447e8e4dfb4ab
Alfie Wilmot <alfred.wilmot@students.plymouth.ac.uk>
2018-12-20 22:35:35 +0000

    Segregating into source code and headers

---
+++ src/Detect_Rectangles/CannyThresholdTrackbar.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include "opencv2/core.hpp"
+#include "opencv2/imgproc.hpp"
+#include "opencv2/highgui.hpp"
+#include <math.h>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+
+#include "CannyThresholdTrackbar.hpp"
+#include "GaussianBlurTrackbar.hpp"
+
+
+/* Gathering I/O frames and display window */
+CannyThresholdTrackbar::CannyThresholdTrackbar(Mat *infrm, Mat *outfrm, String glugg_nafn)
+{
+    this->_input_frame  = infrm;
+    this->_output_frame = outfrm;
+    this->window_name   = glugg_nafn;
+    namedWindow(this->window_name);
+    createTrackbar("Canny Threshold", this->window_name, &this->_thresh, this->_max_thresh, CannyThresholdTrackbar::onCannyTrack, this);
+}
+
+
+
+/* Canny & Sobel Trackbar callbacks */
+void CannyThresholdTrackbar::canny_thresh_callback(int val)
+{
+     this->_thresh = val;
+}
+
+
+/* Processing method */
+void CannyThresholdTrackbar::canny_thresh()
+{
+    cvtColor( *this->_input_frame, *this->_output_frame, CV_BGR2GRAY );
+    blur( *this->_output_frame, *this->_output_frame, Size(3,3) );
+    Canny( *this->_output_frame, *this->_output_frame, this->_thresh, this->_thresh*2, 3);
+
+    imshow(this->window_name, *this->_output_frame);
+}
+
+
+
+
+
+
+
+
+
+
+/* Redirection necessary to setup trackbars within a class */
+
+void CannyThresholdTrackbar::onCannyTrack(int val, void* ptr)
+{
+    CannyThresholdTrackbar* tmp = (CannyThresholdTrackbar*)(ptr);
+    tmp->canny_thresh_callback(val);
+}
+
+
--- src/Detect_Rectangles/GaussianBlurTrackbar.hpp
+++ src/Detect_Rectangles/GaussianBlurTrackbar.hpp
@@ -1,3 +1,6 @@
+#ifndef __GAUSSIANBLURTRACKBAR__
+#define __GAUSSIANBLURTRACKBAR__
+
 #include <iostream>
 #include "opencv2/core.hpp"
 #include "opencv2/imgproc.hpp"
@@ -31,43 +34,4 @@ public:
 };
 
 
-
-/* Gathering I/O frames and display window */
-GaussianBlurTrackbar::GaussianBlurTrackbar(Mat *infrm, Mat *outfrm, String glugg_nafn)
-{
-    this->_input_frame  = infrm;
-    this->_output_frame = outfrm;
-    this->window_name   = glugg_nafn;
-    namedWindow(this->window_name);
-    createTrackbar("Gaussian Blur", this->window_name, &this->_gauss_blur_qty, this->_max_gauss, GaussianBlurTrackbar::onGausTrack, this);
-
-}
-
-/* Redirection necessary to setup trackbar */
-void GaussianBlurTrackbar::onGausTrack(int val, void* ptr)
-{
-    GaussianBlurTrackbar* tmp = (GaussianBlurTrackbar*)(ptr);
-    tmp->gauss_blur_callback(val);
-}
-
-
-
-/* Trackbar callback */
-void GaussianBlurTrackbar::gauss_blur_callback(int val)
-{
-     if (val % 2 == 0)
-     {
-       val = val + 1;
-     }
-     this->_gauss_blur_qty = val;
-}
-
-
-/* Processing methods */
-void GaussianBlurTrackbar::gauss_blur()
-{
-    GaussianBlur(*this->_input_frame, *this->_output_frame, Size(this->_gauss_blur_qty, this->_gauss_blur_qty),0,0);
-    imshow(this->window_name, *this->_output_frame);
-}
-
-
+#endif
